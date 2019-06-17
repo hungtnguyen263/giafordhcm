f0575b4244fe8d36629a54d98e2e18c4640ac16d
Ton Hung Nguyen <tonhungstar@gmail.com>
2019-06-17 07:36:49 +0000

    fix contact info boxes: scroll

--- engines/giafordhcm/public/frontend/js/custom.js
+++ engines/giafordhcm/public/frontend/js/custom.js
@@ -19,15 +19,41 @@ function applyJs(container) {
     }
 }
 
-var box_top_init = $('.contact-info-boxes').offset().top;
-var dis = 160;
-$(window).scroll(function() {
+function fixBox() {
     var box_top = $('.contact-info-boxes').offset().top;
     var window_top = $('body, html').scrollTop() + dis;
     
+    var top;
+    var bottom = '';
+    
     if (window_top > box_top_init) {
-        $('.contact-info-boxes').css('top', (window_top - box_top_init) + 'px');
+        top = (window_top - box_top_init) + 'px';
+        // $('.contact-info-boxes').css('top', (window_top - box_top_init) + 'px');
     } else {
-        $('.contact-info-boxes').css('top', '60px');
+        top = '60px';
+        // $('.contact-info-boxes').css('top', '60px');
     }
+    
+    var ftop = $('footer').offset().top;
+    var bbottom = $('.contact-info-boxes').offset().top + $('.contact-info-boxes').outerHeight();
+    
+    //console.log(ftop - window_top);
+    
+    if (ftop <= (bbottom + 50) && ftop - window_top < 550) {
+        top = '';
+        bottom = '20px';
+    }
+    
+    $('.contact-info-boxes').css('top', top);
+    $('.contact-info-boxes').css('bottom', bottom);
+}
+
+var box_top_init = $('.contact-info-boxes').offset().top;
+var dis = 160;
+$(window).scroll(function() {
+    fixBox();
+});
+
+$(window).ready(function() {
+    setTimeout(function() {fixBox();fixBox();}, 10);
 });
